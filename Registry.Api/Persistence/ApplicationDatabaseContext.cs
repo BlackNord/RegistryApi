d72fe4210980bb43e79db9e4454d8e0c6db2fdca
BlackNord <technic_pr@mail.ru>
2022-02-26 21:46:58 +0300

    registry-api - database configurations

---
+++ Registry.Api/Persistence/ApplicationDatabaseContext.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Registry.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Registry.Api.Persistence
+{
+	public class ApplicationDatabaseContext : DbContext
+	{
+		public DbSet<AppointmentEntity>? Appointments { get; set; }
+
+		public DbSet<DoctorEntity>? Doctors { get; set; }
+
+		public DbSet<PatientEntity>? Patients { get; set; }
+
+		private readonly DatabaseSettings settings;
+
+		public ApplicationDatabaseContext(IOptions<DatabaseSettings> settings)
+		{
+			this.settings = settings.Value;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder
+				.UseNpgsql(settings.ConnectionString)
+				.UseSnakeCaseNamingConvention()
+				.UseLazyLoadingProxies();
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		}
+	}
+}
