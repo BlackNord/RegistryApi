44112468d269fbe115a2fd0d31e4ce35a43cb2a2
BlackNord <technic_pr@mail.ru>
2022-03-10 10:42:24 +0300

    registry-api - integration with billing service was added

---
+++ Registry.Api.Integration/Integration/GCSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Registry.Api.Integration.Integration
+{
+	public class GCSettings
+	{
+		public string? ProjectId { get; set; }
+
+		public string? PublishId { get; set; }
+
+		public string? SubscriptionId { get; set; }
+	}
+}
---
+++ Registry.Api.Integration/Services/Interfaces/IBillingService.cs
@@ -0,0 +1,10 @@
+﻿using Registry.Api.Integration.Dto.Requests;
+using Registry.Api.Integration.Dto.Responses;
+
+namespace Registry.Api.Integration.Service.Interfaces
+{
+	public interface IBillingService
+	{
+		Task<AssignResponse> Assign(AssignRequest request);
+	}
+}
